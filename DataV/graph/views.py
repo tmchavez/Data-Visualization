b7ccf120b5b39715a2b84226083edf2b2726db0a
Taylor Chavez <tmchavez@ucsc.edu>
2018-11-19 22:02:38 -0800

    database info on frontpage

--- DataV/graph/views.py
+++ DataV/graph/views.py
@@ -5,9 +5,22 @@ from django.contrib.auth.models import User
 
 # Create your views here.
 
+datas = [
+  {
+    'author' : 'tmchavez',
+    'title' : 'test 1',
+    'content' : '1 4 7 9 5',
+  },
+  {
+    'author' : 'tmchavez',
+    'title' : 'test 2',
+    'content' : '11 14 27 49 35',
+  }
+]
+
 def home(request):
   context = {
-    'datasets': dataO.objects.all()
+    'datasets' : dataO.objects.all()
   }
   return render(request, 'graph/frontpage.html', context)
 
