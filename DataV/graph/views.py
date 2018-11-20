690d905df5726e2ae165f79d273e72f7818fb395
Taylor Chavez <tmchavez@ucsc.edu>
2018-11-19 20:18:33 -0800

    : issa vibe

--- DataV/graph/views.py
+++ DataV/graph/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import dataO
+from django.contrib.auth.models import User
 
 # Create your views here.
 
 def home(request):
-  return render(request, 'graph/frontpage.html')
+  context = {
+    'datasets': dataO.objects.all()
+  }
+  return render(request, 'graph/frontpage.html', context)
 
 def double(request):
   return render(request, 'graph/double.html')
