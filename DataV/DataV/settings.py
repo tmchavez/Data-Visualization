89a2176a1245ecfcc818d168ad40aeb800f03b41
Taylor Chavez <tmchavez@ucsc.edu>
2018-11-27 11:01:51 -0800

    added user registration page

--- DataV/DataV/settings.py
+++ DataV/DataV/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'graph.apps.GraphConfig',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
