ae3a3f1fc3d8fe91799e603c5a338ceabe5b20bb
Frank Kohn <fkohn@ucsc.edu>
2018-11-29 13:22:57 -0800

    no loops

--- Combined/js/osc.js
+++ Combined/js/osc.js
@@ -5,6 +5,8 @@ var arrayPlay;
 var highFreq = 800;
 var lowFreq = 200;
 
+Tone.Transport.bpm.value = 23;
+
 function togglePlay(){
   if($('#toggle-play').is(':checked')){
     freq = 440;
@@ -21,27 +23,24 @@ function playSeries(arr){
   Tone.Transport.stop();
   Tone.Master.volume.rampTo(0, 0.1);
   Tone.Transport.start('+0.1');
+  console.log("playing");
 }
 
 function clearSeries(){
   arrayPlay.removeAll();
 }
 
-// function initializeSeries(){
-//   for(var i=0; i<arr.length; i++){
-//       Tone.Transport.scheduleOnce(function(time){
-//         piano.triggerAttackRelease(arr[i], '16n', time)
-//       }, i);
-//    }
-//    console.log(arr);
-// }
-
 function initializeSeries(arr){
   var toneArr = scaleArray(arr);
 
   arrayPlay = new Tone.Sequence(function(time, note){
-      piano.triggerAttackRelease(note, "16n", time);
-  }, toneArr, "2n").start(0).stop(arr.length*2);
+      piano.triggerAttackRelease(note, "32n", time);
+  }, toneArr, "4n").start(0);
+
+  arrayPlay.loop = 0;
+
+  console.log("intialize done");
+  console.log(toneArr);
 }
 
 function scaleArray(arr){
@@ -64,15 +63,7 @@ var piano = new Tone.PolySynth(4, Tone.Synth, {
     "portamento" : 0.05
 }).toMaster()
 
-Tone.Transport.bpm.value = 60;
-
 
 $(function(){
   Tone.Master.volume.rampTo(-Infinity, 0.05);
 });
-
-//function inputArray(){
-//  arr = $('#inputArray').val().split(" ");
-//  initializeSeries();
-//  playSeries();
-//}
