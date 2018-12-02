b85ad1318925f85b65f341799d68bc0a80c7473f
Taylor Chavez <tmchavez@ucsc.edu>
2018-12-01 18:04:52 -0800

    : a great success!!!!

--- DataV/graph/models.py
+++ DataV/graph/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your models here.
 class dataO(models.Model):
@@ -9,3 +10,6 @@ class dataO(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('graph-home')
--- DataV/users/views.py
+++ DataV/users/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from graph.models import dataO
 from .forms import UserRegisterForm
+
 
 def register(request):
     if request.method == 'POST':
@@ -8,9 +11,16 @@ def register(request):
       if form.is_valid():
         form.save()
         username = form.cleaned_data.get('username')
-        messages.success(request, f'Account created for {username}!')
-        return redirect('graph-home')
+        messages.success(request, f'Account created for {username}! login')
+        return redirect('login')
 
     else:
       form = UserRegisterForm()
     return render(request, 'users/reg.html', {'form': form})
+
+@login_required
+def profile (request):
+    context = {
+      'datasets' : dataO.objects.all()
+    }
+    return render(request, 'users/profile.html', context)
